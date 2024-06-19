a1c786c5a4807759a24bad2e11145dad6acfe68e
tpring <pororo1@naver.com>
2024-06-19 12:01:28 +0900

    update: supabase유호성 및 ui 수정

--- src/components/sidebar/SideBarCourse.jsx
+++ src/components/sidebar/SideBarCourse.jsx
@@ -5,14 +5,16 @@ import Sidebar from "./Sidebar";
 import { useRecoilState } from "recoil";
 import { createCourse } from "../../lib/api/course";
 import { selectPlaceState } from "../../recoil/atom/searchAtom";
+import FixedButton from "../common/FixedButton";
 
 const SideBarCourse = ({ isCourseOpen, onCourseClose, isOpen, onClose, openSidebar }) => {
   const [coursePlaces, setCoursePlaces] = useRecoilState(selectPlaceState);
 
   const handleSavePlaces = async () => {
     await createCourse(coursePlaces);
+    setCoursePlaces([]);
   };
-
+  // 장소를 삭제하는 함수
   const handleDeletePlace = (idToDelete) => {
     const updatedPlaces = coursePlaces.filter((place) => place.id !== idToDelete);
     setCoursePlaces(updatedPlaces);
@@ -24,43 +26,42 @@ const SideBarCourse = ({ isCourseOpen, onCourseClose, isOpen, onClose, openSideb
         isCourseOpen ? "translate-x-0" : "translate-x-full"
       }`}
     >
-      <div className="flex items-center justify-between">
+      <div className="flex items-center justify-between p-4 border-b">
         <button onClick={onCourseClose}>
           <img className="w-[50px] h-[50px]" src={closeBtn} alt="close button" />
         </button>
-        <button
-          className="mx-3 relative mt-4 w-[130px] py-2 text-lg bg-blue-500 text-white rounded-lg"
-          onClick={handleSavePlaces}
-        >
-          저장 하기
-        </button>
       </div>
-      <div className="w-[428px] h-auto mx-auto text-center">
+      <div className="w-[428px] h-auto mx-auto text-center p-4">
         <h2 className="mt-4 text-xl font-semibold">코스 정하기</h2>
-        <div className="mt-4">
-          {coursePlaces.map((place) => (
-            <div key={place.id}>
-              <div className="flex items-center justify-between bg-white p-4">
-                <PlacesItem place={place} />
-                <button onClick={() => handleDeletePlace(place.id)}>
-                  <img className="w-[25px] h-[25px] mr-2" src={closeBtn} alt="close button" />
-                </button>
-              </div>
-              <div className="my-2 text-gray-400">▼</div>
+      </div>
+      <div
+        className="overflow-y-auto h-[calc(100%-130px)] p-4 w-[428px] mx-auto text-center"
+        style={{ scrollbarWidth: "none", msOverflowStyle: "none" }}
+      >
+        <style>{`.overflow-y-auto::-webkit-scrollbar {display: none;}`}</style>
+        {coursePlaces.map((place) => (
+          <div key={place.id}>
+            <div className="flex items-center justify-between bg-white p-4">
+              <PlacesItem place={place} />
+              <button onClick={() => handleDeletePlace(place.id)}>
+                <img className="w-[25px] h-[25px] mr-2" src={closeBtn} alt="close button" />
+              </button>
             </div>
-          ))}
-
-          <div className="my-5">
-            <button
-              className="w-[300px] text-2xl h-12 border-2 border-gray-300 rounded-[10px] bg-gray-100"
-              onClick={openSidebar}
-            >
-              +
-            </button>
+            <div className="my-2 text-gray-400">▼</div>
           </div>
+        ))}
+
+        <div className="my-5">
+          <button
+            className="w-[300px] text-2xl h-12 border-2 border-gray-300 rounded-[10px] bg-gray-100 mb-[100px]"
+            onClick={openSidebar}
+          >
+            +
+          </button>
         </div>
       </div>
-      {isOpen && <Sidebar isOpen={isOpen} onClose={onClose} />}
+      <Sidebar isOpen={isOpen} onClose={onClose} />
+      <FixedButton text="저장 하기" onClick={handleSavePlaces} />
     </div>
   );
 };
--- src/lib/api/course.js
+++ src/lib/api/course.js
@@ -1,11 +1,20 @@
 import supabase from "../../supabase/supabase";
 
 export const createCourse = async (coursePlaces) => {
+  // 만약 coursePlaces 배열이 비어 있다면
+  if (coursePlaces.length === 0) {
+    alert("코스를 등록해주시기 바랍니다.");
+    return;
+  }
+  // 현재 로그인한 사용자 정보 가져오기
   const {
     data: { user },
   } = await supabase.auth.getUser();
+
+  // course 테이블에 사용자 ID를 포함하여 코스 정보 삽입 후 결과 가져오기
   const result = await supabase.from("course").insert({ user_id: user.id }).select();
 
+  // 각 장소(place)에 대해 코스 정보를 생성하는 함수 호출
   for (const place of coursePlaces) {
     const placesData = {
       course_id: result.data[0].id,
@@ -18,7 +27,9 @@ export const createCourse = async (coursePlaces) => {
       x: place.x,
       y: place.y,
     };
+
     await createCoursePlaces(placesData);
+    alert("저장에 성공하셨습니다");
   }
 };
 
