008847647828b9886f36dd0ee0440d3648cb4f64
DESKTOP-SUURT89\SeYoung <parksy4073@naver.com>
2024-06-19 11:41:41 +0900

    my page

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
 import ExamplePage from "../pages/ExamplePage";
+import Mypage from "../pages/Mypage";
 
 const router = createBrowserRouter([
   {
@@ -9,6 +10,10 @@ const router = createBrowserRouter([
       {
         path: "/example",
         element: <ExamplePage />,
+      },
+      {
+        path: "/mypage", 
+        element: <Mypage />,  
       },
     ],
   },
