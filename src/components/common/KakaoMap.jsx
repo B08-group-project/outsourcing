7322f431426cd78128328ad19ac5129722997899
2hanbyeol1 <2hanbyeol1@naver.com>
2024-06-19 17:12:18 +0900

    update: 선택한 경로에 해당하는 마커 추가

--- src/components/common/KakaoMap.jsx
+++ src/components/common/KakaoMap.jsx
@@ -1,19 +1,20 @@
 import { useEffect, useState } from "react";
-import { Map, MapMarker, CustomOverlayMap } from "react-kakao-maps-sdk";
+import { Map, MapMarker } from "react-kakao-maps-sdk";
 import { useRecoilValue, useSetRecoilState } from "recoil";
-import { searchKeywordState, searchCategoryState, searchData } from "../../recoil/atom/searchAtom";
+import { MARKER_IMG } from "../../constants/Category";
+import { searchCategoryState, searchData, searchKeywordState, selectPlaceState } from "../../recoil/atom/searchAtom";
 
 const { kakao } = window;
 
 function KakaoMap() {
   const [level, setLevel] = useState(3);
   const [info, setInfo] = useState();
-  const [markers, setMarkers] = useState([]);
   const [map, setMap] = useState();
-  const [location, setLoacation] = useState(null);
+  const [location, setLocation] = useState(null);
   const keyword = useRecoilValue(searchKeywordState);
   const category = useRecoilValue(searchCategoryState);
   const setSearchData = useSetRecoilState(searchData);
+  const markers = useRecoilValue(selectPlaceState);
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(successHandler, errorHandler);
@@ -21,34 +22,24 @@ function KakaoMap() {
 
   useEffect(() => {
     if (!map) return;
+    if (!keyword) return;
     const ps = new kakao.maps.services.Places();
 
     const options = {
       category_group_code: category,
     };
-    // CT1 : 영화관, AT4 : 관광명소, FD6 : 식당 , CE7 : 카페
 
     ps.keywordSearch(
       keyword,
       (data, status) => {
         if (status === kakao.maps.services.Status.OK) {
           const bounds = new kakao.maps.LatLngBounds();
-          let markers = [];
           const checkedData = data.map((item) => ({ ...item, checked: false }));
           setSearchData(checkedData);
 
           for (var i = 0; i < data.length; i++) {
-            markers.push({
-              position: {
-                lat: data[i].y,
-                lng: data[i].x,
-              },
-              content: data[i].place_name,
-            });
-
             bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
           }
-          setMarkers(markers);
 
           map.setBounds(bounds);
         }
@@ -59,7 +50,7 @@ function KakaoMap() {
 
   const successHandler = (response) => {
     const { latitude, longitude } = response.coords;
-    setLoacation({ latitude, longitude });
+    setLocation({ latitude, longitude });
   };
 
   const errorHandler = (error) => {
@@ -75,12 +66,13 @@ function KakaoMap() {
     >
       {markers.map((marker) => (
         <MapMarker
-          key={`marker-${marker.content}-${marker.position.lat},${marker.position.lng}`}
-          position={marker.position}
-          onClick={() => setInfo(marker)}
-        >
-          {info && info.content === marker.content && <div style={{ color: "#000" }}>{marker.content}</div>}
-        </MapMarker>
+          key={`marker-${marker.id}`}
+          position={{ lat: marker.y, lng: marker.x }}
+          image={{
+            src: MARKER_IMG[marker.category_group_code],
+            size: { width: 30, height: 30 },
+          }}
+        />
       ))}
       <button onClick={() => setLevel(level + 1)}>-</button>
       <button onClick={() => setLevel(level - 1)}>+</button>
---
+++ src/constants/Category.js
@@ -0,0 +1,12 @@
+import attractionMarker from "../assets/attraction-marker.png";
+import cafeMarker from "../assets/cafe-marker.png";
+import cultureMarker from "../assets/culture-marker.png";
+import restaurantMarker from "../assets/restaurant-marker.png";
+
+// CT1 : 영화관, AT4 : 관광명소, FD6 : 식당 , CE7 : 카페
+export const MARKER_IMG = {
+  CT1: cultureMarker,
+  CE7: cafeMarker,
+  FD6: restaurantMarker,
+  AT4: attractionMarker,
+};
